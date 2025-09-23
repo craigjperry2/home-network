c728ec88cb26ebde50101dcee93115162b88f2b6
Craig Perry <craigp84@gmail.com>
2025-09-23 19:50:16 +0100

    feat(nix): Import d2 macos 26 config

---
+++ nix/d2/flake.nix
@@ -0,0 +1,107 @@
+{
+  description = "Example nix-darwin system flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-25.05-darwin";
+
+    nix-darwin.url = "github:nix-darwin/nix-darwin/nix-darwin-25.05";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    nix-homebrew.url = "github:zhaofengli/nix-homebrew";
+    homebrew-core = {
+      url = "github:homebrew/homebrew-core";
+      flake = false;
+    };
+    homebrew-cask = {
+      url = "github:homebrew/homebrew-cask";
+      flake = false;
+    };
+  };
+
+  outputs = inputs@{ self, nixpkgs, nix-darwin, home-manager, nix-homebrew, homebrew-core, homebrew-cask, ... }:
+  let
+    configuration = { pkgs, ... }: {
+      # List packages installed in system profile. To search by name, run:
+      # $ nix-env -qaP | grep wget
+      environment.systemPackages = [ pkgs.vim ];
+
+      system.primaryUser = "craig";
+      
+      homebrew = {
+          enable = true;
+          taps = [];
+          brews = [];
+          casks = [
+            "audacity"
+            "chatgpt"
+            # "citrix-workspace"
+            "claude"
+            "docker-desktop"
+            "font-jetbrains-mono-nerd-font"
+            "firefox@developer-edition"
+            "iina"
+            "istat-menus@6"
+            "lm-studio"
+            "obsidian"
+            "visual-studio-code"
+          ];
+      };
+
+      # Necessary for using flakes on this system.
+      nix.settings.experimental-features = "nix-command flakes";
+
+      # Enable alternative shell support in nix-darwin.
+      # programs.fish.enable = true;
+
+      # Set Git commit hash for darwin-version.
+      system.configurationRevision = self.rev or self.dirtyRev or null;
+
+      # Used for backwards compatibility, please read the changelog before changing.
+      # $ darwin-rebuild changelog
+      system.stateVersion = 6;
+
+      # The platform the configuration will be used on.
+      nixpkgs.hostPlatform = "aarch64-darwin";
+    };
+  in
+  {
+    # Build darwin flake using:
+    # $ darwin-rebuild build --flake .#d2
+    darwinConfigurations."d2" = nix-darwin.lib.darwinSystem {
+      modules = [
+        home-manager.darwinModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          users.users.craig.home = "/Users/craig";
+          home-manager.users.craig = import ./home.nix;
+          
+          # Pass inputs to home-manager modules
+          home-manager.extraSpecialArgs = { inherit inputs; };
+        }
+        nix-homebrew.darwinModules.nix-homebrew
+        {
+          nix-homebrew = {
+            user = "craig";
+            enable = true;
+            taps = {
+              "homebrew/homebrew-core" = homebrew-core;
+              "homebrew/homebrew-cask" = homebrew-cask;
+            };
+            mutableTaps = false;
+            autoMigrate = true;
+          };
+        }
+        ({config, ...}: {
+          homebrew.taps = builtins.attrNames config.nix-homebrew.taps;
+        })
+        configuration
+      ];
+    };
+  };
+}
