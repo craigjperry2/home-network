733076a6f80acccbfe24f6d0301aff5d74fd9d49
Craig Perry <craigp84@gmail.com>
2025-10-26 10:49:58 +0000

    feat(nix): direnv python uv auto setup

--- nix/d2/home.nix
+++ nix/d2/home.nix
@@ -45,6 +45,28 @@
   programs.direnv = {
     enable = true;
     enableZshIntegration = true;
+    stdlib = ''
+      layout_uv() {
+          if [[ -d ".venv" ]]; then
+              VIRTUAL_ENV="$(pwd)/.venv"
+          fi
+
+          if [[ -z $VIRTUAL_ENV || ! -d $VIRTUAL_ENV ]]; then
+              log_status "No virtual environment exists. Executing \`uv venv\` to create one."
+              uv venv
+              VIRTUAL_ENV="$(pwd)/.venv"
+          fi
+
+          uv sync
+
+          if [ -d ".venv/bin" ]; then
+              PATH_add .venv/bin
+          fi
+
+          export UV_ACTIVE=1  # or VENV_ACTIVE=1
+          export VIRTUAL_ENV
+      }
+    '';
   };
 
   programs.fzf = {
@@ -423,6 +445,10 @@ END
 
       directory = {
         read_only = " 󰌾";
+      };
+
+      direnv = {
+        disabled = false;
       };
 
       docker_context = {
