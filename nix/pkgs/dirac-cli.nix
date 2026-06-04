ff9250441283e42e6f185dbe3e2b9941326b14bc
Craig Perry <craigp84@gmail.com>
2026-06-04 15:53:17 +0100

    fix(nix): dirac deriv missing python, simplified with jq

--- nix/pkgs/dirac-cli.nix
+++ nix/pkgs/dirac-cli.nix
@@ -5,6 +5,7 @@
   nodejs_24,
   versionCheckHook,
   writableTmpDirAsHomeHook,
+  jq,
 }:
 buildNpmPackage {
   pname = "dirac-cli";
@@ -12,23 +13,18 @@ buildNpmPackage {
 
   nodejs = nodejs_24;
 
+  nativeBuildInputs = [jq];
+
   src = fetchzip {
     url = "https://registry.npmjs.org/dirac-cli/-/dirac-cli-0.3.44.tgz";
     hash = "sha256-ooMfVhPIH8SDD1IGAkry4wjqavX3QlFYZw43poPAKj4=";
   };
 
   postPatch = ''
-        cp ${./dirac-cli-package-lock.json} package-lock.json
+    cp ${./dirac-cli-package-lock.json} package-lock.json
 
-        python - <<'PY'
-    import json
-    from pathlib import Path
-
-    path = Path("package.json")
-    data = json.loads(path.read_text())
-    data.pop("man", None)
-    path.write_text(json.dumps(data, indent=2) + "\n")
-    PY
+    ${jq}/bin/jq 'del(.man)' package.json > package.json.tmp
+    mv package.json.tmp package.json
   '';
 
   npmDepsHash = "sha256-Aqu59AdaFcknTofGdY+5O2apBkTbZSiVdvWP2b11VDQ=";
